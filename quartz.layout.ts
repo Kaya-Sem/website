34207e0a47ebb8c8e47990dccf129d77b2adba64
Kaya-Sem <kayasemvc@gmail.com>
2024-07-15 15:24:38 +0200

    update footer

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -1,20 +1,19 @@
-import { PageLayout, SharedLayout } from "./quartz/cfg"
+ihttps://github.com/jackyzha0/quartzmport { PageLayout, SharedLayout } from "./quartz/cfg"
 import * as Component from "./quartz/components"
 
-// components shared across all pages
+// omponents shared across all pages
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
   header: [],
   afterBody: [],
   footer: Component.Footer({
     links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
+      GitHub: "https://github.com/Kaya-Sem"
     },
   }),
 }
 
-// components for pages that display a single page (e.g. a single note)
+// Components for pages that display a single page (e.g. a single note)
 export const defaultContentPageLayout: PageLayout = {
   beforeBody: [
     Component.Breadcrumbs(),
@@ -36,7 +35,7 @@ export const defaultContentPageLayout: PageLayout = {
   ],
 }
 
-// components for pages that display lists of pages  (e.g. tags or folders)
+// Components for pages that display lists of pages (e.g. tags or folders)
 export const defaultListPageLayout: PageLayout = {
   beforeBody: [Component.Breadcrumbs(), Component.ArticleTitle(), Component.ContentMeta()],
   left: [
